69e8f7b0c571d834057c0a4c342c782afef63607
FrozenIce0617 <prosdev087@gmail.com>
2020-02-03 19:40:25 +0000

    Add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => {
+  return (
+    <div className="navbar navbar-fixed-top">
+      <div className="navbar-right" />
+    </div>
+  );
+};
+
+export default Header;
